7f9d527f50bd751d5145dbed5f897514a7e2ec5f
Michael Bromley <michael@michaelbromley.co.uk>
2020-08-19 16:25:57 +0200

    fix: Remove reference to process.env

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiHost: process.env.STOREFRONT_API_HOST || 'http://localhost',
-    apiPort: process.env.STOREFRONT_API_PORT || 3000,
+    apiHost: 'http://localhost',
+    apiPort: 3000,
     shopApiPath: 'shop-api',
     baseHref: '/',
 };
