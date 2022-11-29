4520a3aafd2c0131e1f2303be498a7576abd18bf
Michael Bromley <michael@michaelbromley.co.uk>
2022-11-29 13:13:48 +0100

    Improve product loading state

--- src/app/core/components/asset-gallery/asset-gallery.component.ts
+++ src/app/core/components/asset-gallery/asset-gallery.component.ts
@@ -1,9 +1,10 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, Input, OnChanges, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
 
 import { AssetFragment } from '../../../common/generated-types';
 
 import './types.d';
+import { isPlatformBrowser } from '@angular/common';
 
 export type AssetWithDimensions = Pick<AssetFragment, 'id' | 'preview' | 'width' | 'height'>;
 
@@ -12,49 +13,71 @@ export type AssetWithDimensions = Pick<AssetFragment, 'id' | 'preview' | 'width'
     templateUrl: './asset-gallery.component.html',
     styleUrls: ['./asset-gallery.component.scss'],
 })
-export class AssetGalleryComponent implements OnInit, AfterViewInit {
-    @Input() assets: AssetWithDimensions[] = [];
+export class AssetGalleryComponent implements OnInit, OnChanges, AfterViewInit {
+    @Input() assets?: AssetWithDimensions[] = [];
     @Input() selectedAssetId: string;
     @ViewChild('mainPreview', {static: false})
+    featuredAssetLoaded = false;
     private mainPreview: ElementRef<HTMLImageElement>;
 
     selectedAsset?: AssetWithDimensions;
     private gallery: any;
 
-    @ViewChild('photoswipeModal', {static: true}) modal: ElementRef<HTMLDivElement>;
-
-    constructor(private elementRef: ElementRef) {
+    constructor(@Inject(PLATFORM_ID) private platformId: any) {
     }
 
     ngOnInit() {
         this.selectImage(this.selectedAssetId);
     }
 
+    ngOnChanges() {
+        if (this.assets) {
+            this.initPhotoswipe();
+            this.selectImage(this.selectedAssetId);
+        }
+    }
+
     ngAfterViewInit() {
-        const items = this.assets.map(asset => ({
-            src: asset.preview,
-            msrc: asset.preview + '?preset=medium',
-            width: asset.width || 1000,
-            height: asset.height || 1000,
-        }));
-        this.gallery = new PhotoSwipeLightbox({
-            dataSource: items,
-            pswpModule: () => import('photoswipe'),
-            showHideOpacity: true,
-        });
-        this.gallery.init();
-        console.log(this.gallery);
+        if (this.assets) {
+            this.initPhotoswipe();
+        }
+    }
+
+    onLoad() {
+        console.log(`loaded`);
+        this.featuredAssetLoaded = true;
+
+    }
+
+    private initPhotoswipe() {
+        if (isPlatformBrowser(this.platformId)) {
+            const items = this.assets?.map(asset => ({
+                src: asset.preview,
+                msrc: asset.preview + '?preset=medium',
+                width: asset.width || 1000,
+                height: asset.height || 1000,
+            }));
+            this.gallery = new PhotoSwipeLightbox({
+                dataSource: items,
+                pswpModule: () => import('photoswipe'),
+                showHideOpacity: true,
+            });
+            this.gallery.init();
+        }
     }
 
     selectImage(assetId: string) {
         if (assetId != null) {
-            this.selectedAsset = this.assets.find(a => a.id === assetId);
+            this.selectedAsset = this.assets?.find(a => a.id === assetId);
         } else {
-            this.selectedAsset = this.assets[0];
+            this.selectedAsset = this.assets?.[0];
         }
     }
 
     openImage(assetId: string) {
+        if (!this.assets) {
+            return;
+        }
         const index = this.assets.findIndex(a => a.id === assetId);
         this.gallery.loadAndOpen(index);
     }
--- src/app/core/components/collection-breadcrumbs/collection-breadcrumbs.component.ts
+++ src/app/core/components/collection-breadcrumbs/collection-breadcrumbs.component.ts
@@ -5,12 +5,12 @@ import { GetCollectionQuery } from '../../../common/generated-types';
 @Component({
     selector: 'vsf-collection-breadcrumbs',
     templateUrl: './collection-breadcrumbs.component.html',
-    // styleUrls: ['./collection-breadcrumbs.component.scss'],
+    styleUrls: ['./collection-breadcrumbs.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CollectionBreadcrumbsComponent {
 
-    @Input() breadcrumbs: NonNullable<GetCollectionQuery['collection']>['breadcrumbs'] = [];
+    @Input() breadcrumbs?: NonNullable<GetCollectionQuery['collection']>['breadcrumbs'] = [];
     @Input() linkLast = false;
 
     tail<T>(arr: T[] | null): T[] {
